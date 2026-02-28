8dff2617ce251c59d86ca1619da6193aa67b1aa3
skies-starred <skies.starred@gmail.com>
2026-02-28 04:36:29 +0000

    fix(LavaFogEnvironmentMixin): 1.21.11

--- src/main/java/xyz/aerii/athen/mixin/mixins/LavaFogEnvironmentMixin.java
+++ src/main/java/xyz/aerii/athen/mixin/mixins/LavaFogEnvironmentMixin.java
@@ -4,18 +4,27 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.fog.FogData;
 import net.minecraft.client.renderer.fog.environment.LavaFogEnvironment;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.aerii.athen.modules.impl.render.RenderOptimiser;
 
+//? if >= 1.21.11 {
+/*import net.minecraft.client.Camera;
+*///? } else {
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+//? }
+
 @Mixin(LavaFogEnvironment.class)
 public class LavaFogEnvironmentMixin {
     @Inject(method = "setupFog", at = @At("HEAD"), cancellable = true)
+    //? if >= 1.21.11 {
+    /*private void athen$setupFog(FogData fogData, Camera camera, ClientLevel level, float renderDistance, DeltaTracker deltaTracker, CallbackInfo ci) {
+    *///? } else {
     private void athen$setupFog(FogData fogData, Entity entity, BlockPos pos, ClientLevel level, float renderDistance, DeltaTracker deltaTracker, CallbackInfo ci) {
+    //? }
         if (!RenderOptimiser.getLava()) return;
         fogData.environmentalStart = Float.MAX_VALUE;
         fogData.environmentalEnd = Float.MAX_VALUE;
