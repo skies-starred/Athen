86fb56ccd75b36086323a6351aefd3aeb0f724fb
Gaeritag <125013771+Gaeritag@users.noreply.github.com>
2026-03-01 09:59:33 +0100

    feat(MarkLobbies)
    Co-authored-by: skies-starred <skies.starred@gmail.com>

---
+++ src/main/kotlin/xyz/aerii/athen/modules/impl/general/MarkLobbies.kt
@@ -0,0 +1,24 @@
+package xyz.aerii.athen.modules.impl.general
+
+import xyz.aerii.athen.annotations.Load
+import xyz.aerii.athen.config.Category
+import xyz.aerii.athen.events.LocationEvent
+import xyz.aerii.athen.handlers.Typo.modMessage
+import xyz.aerii.athen.modules.Module
+
+@Load
+object MarkLobbies: Module(
+    "Mark lobbies",
+    "Marks lobbies and alerts you if you have already been inside that lobby.",
+    Category.GENERAL
+) {
+    private val onlyCrystalHollows by config.switch("Only in Crystal Hollows")
+    private val lobbies = mutableSetOf<String>()
+
+    init {
+        on<LocationEvent.ServerChange> {
+            if (mode != "crystal_hollows" && onlyCrystalHollows) return@on
+            if (!lobbies.add(name)) "You've been in this lobby!".modMessage()
+        }
+    }
+}
