d2052696d741b193c7e58aaef7a962af89eee64e
skies-starred <skies.starred@gmail.com>
2026-03-13 05:26:58 +0000

    internal(SlayerAPI): current slayer var

--- src/main/kotlin/xyz/aerii/athen/api/skyblock/SlayerAPI.kt
+++ src/main/kotlin/xyz/aerii/athen/api/skyblock/SlayerAPI.kt
@@ -1,9 +1,8 @@
 package xyz.aerii.athen.api.skyblock
 
 import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.decoration.ArmorStand
-import net.minecraft.world.entity.monster./*? >= 1.21.11 { *//*spider.*//*? }*/CaveSpider
 import tech.thatgravyboat.skyblockapi.api.area.slayer.*
+import xyz.aerii.athen.accessors.parent
 import xyz.aerii.athen.annotations.Priority
 import xyz.aerii.athen.events.EntityEvent
 import xyz.aerii.athen.events.LocationEvent
@@ -13,18 +12,20 @@ import xyz.aerii.athen.events.core.on
 import xyz.aerii.athen.handlers.Typo.devMessage
 import xyz.aerii.athen.handlers.Typo.stripped
 import java.util.*
-import kotlin.math.abs
 
 // FIXME: Doesn't work for tarantula T5.
 @Priority
 object SlayerAPI {
-    private val logged: MutableSet<Entity> = mutableSetOf()
-    val slayerBosses: WeakHashMap<Entity, SlayerInfo> = WeakHashMap()
-    val SLAYER_NAMES = SLAYER_MOBS.flatMap { it.inGameNames }.toSet()
-
     private val questFailedRegex = Regex("\\s+SLAYER QUEST FAILED!")
     private val questStartedRegex = Regex("\\s+SLAYER QUEST STARTED!")
     private val questCompletedRegex = Regex("\\s+SLAYER QUEST COMPLETE!")
+
+    private val logged: MutableSet<Entity> = mutableSetOf()
+    val slayerBosses: WeakHashMap<Entity, SlayerInfo> = WeakHashMap()
+    val slayerNames = SLAYER_MOBS.flatMap { it.inGameNames }.toSet()
+
+    var slayer: SlayerInfo? = null
+        private set
 
     init {
         on<MessageEvent.Chat.Receive> {
@@ -37,17 +38,19 @@ object SlayerAPI {
                 questCompletedRegex.matches(stripped) -> {
                     "SlayerAPI: Quest completed!".devMessage()
                     SlayerEvent.Quest.End.post()
+                    slayer = null
                 }
 
                 questFailedRegex.matches(stripped) -> {
                     "SlayerAPI: Quest failed!".devMessage()
                     SlayerEvent.Cleanup(SlayerEvent.CleanupType.QuestFail).post()
+                    slayer = null
                 }
             }
         }
 
         on<EntityEvent.Update.Attach> {
-            val entity = infoLineEntity.c() ?: return@on
+            var entity = entity.parent ?: return@on
 
             val slayerInfo = when {
                 component.stripped().check() -> slayerBosses.computeIfAbsent(entity, ::SlayerInfo)
@@ -61,6 +64,7 @@ object SlayerAPI {
             ) {
                 when (slayerInfo.type) {
                     is SlayerType -> {
+                        if (slayerInfo.isOwnedByPlayer) slayer = slayerInfo
                         SlayerEvent.Boss.Spawn(entity, slayerInfo).post()
                         "SlayerAPI: Slayer spawned (owner=${slayerInfo.owner}, tier=${slayerInfo.tier}, tickAge=${entity.tickCount / 20.0}s)".devMessage()
                     }
@@ -84,6 +88,7 @@ object SlayerAPI {
 
             when (slayerInfo.type) {
                 is SlayerType -> {
+                    if (slayerInfo.isOwnedByPlayer) slayer = null
                     SlayerEvent.Boss.Death(entity, slayerInfo).post()
                     "SlayerAPI: Slayer killed (owner=${slayerInfo.owner}, tier=${slayerInfo.tier}, tickAge=${entity.tickCount / 20.0}s)".devMessage()
                 }
@@ -110,46 +115,13 @@ object SlayerAPI {
     private fun reset() {
         slayerBosses.clear()
         logged.clear()
+        slayer = null
     }
 
     private fun String.check(): Boolean {
         if (!startsWith("☠") && !endsWith("❤") && !endsWith("❤ ✯") && !endsWith(" Hits")) return false
 
-        for (name in SLAYER_NAMES) if (contains(name)) return true
+        for (name in slayerNames) if (contains(name)) return true
         return false
     }
-
-    /**
-     * Finds the best matching entity that's closest to this entity.
-     * If two entities that are at the same distance is found, it prefers the smallest tick difference.
-     * @return best entity
-     */
-    private fun Entity.c(): Entity? {
-        val entities = level().getEntities(this, boundingBox.inflate(0.0, 1.0, 0.0))
-
-        var b: Entity? = null
-        var d = Double.MAX_VALUE
-        var t = Int.MAX_VALUE
-
-        for (e in entities) {
-            if (!e.v()) continue
-
-            val dist = e.distanceToSqr(this)
-            if (b != null && dist > d) continue
-
-            val tick = abs(e.tickCount - tickCount)
-            if (dist == 0.0 && tick == 0) return e
-
-            if (b == null || dist < d || (dist == d && tick < t)) {
-                b = e
-                d = dist
-                t = tick
-            }
-        }
-
-        return b
-    }
-
-    private fun Entity?.v(): Boolean =
-        this != null && this !is ArmorStand && this !is CaveSpider && isAlive && !isInvisible
 }
