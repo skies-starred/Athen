274a767bf7432fcf744fa92f675c0b954b12ad0b
skies-starred <skies.starred@gmail.com>
2026-04-14 06:16:46 +0100

    feat(ItemRarityBackground): options

--- src/main/kotlin/xyz/aerii/athen/modules/impl/render/ItemRarityBackground.kt
+++ src/main/kotlin/xyz/aerii/athen/modules/impl/render/ItemRarityBackground.kt
@@ -2,6 +2,8 @@
 
 package xyz.aerii.athen.modules.impl.render
 
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.data.SkyBlockRarity
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
@@ -9,9 +11,11 @@ import xyz.aerii.athen.annotations.Load
 import xyz.aerii.athen.annotations.OnlyIn
 import xyz.aerii.athen.config.Category
 import xyz.aerii.athen.events.GuiEvent
+import xyz.aerii.athen.events.core.runWhen
 import xyz.aerii.athen.modules.Module
 import xyz.aerii.athen.utils.render.Render2D.drawOutline
 import xyz.aerii.athen.utils.render.Render2D.drawRectangle
+import xyz.aerii.library.handlers.Observable.Companion.and
 import xyz.aerii.library.utils.withAlpha
 import java.awt.Color
 
@@ -23,7 +27,9 @@ object ItemRarityBackground : Module(
     Category.RENDER
 ) {
     private val renderStyle by config.dropdown("Render style", listOf("Filled", "Outline",  "Filled outline"), 2)
-    private val fillAlpha by config.slider("Fill alpha", 0.5f, 0f, 1f, showDouble = true).dependsOn { renderStyle == 0 || renderStyle == 2 }
+    private val mode = config.dropdown("Render mode", listOf("Everywhere", "Slots"), 1).custom("mode")
+    private val hotbar = config.switch("Hotbar", true).dependsOn { mode.value == 1 }.custom("hotbar")
+    private val fill by config.slider("Fill alpha", 0.5f, 0f, 1f, showDouble = true).dependsOn { renderStyle == 0 || renderStyle == 2 }
 
     private val colorExpandable by config.expandable("Colors")
     private val `color$common` by config.colorPicker("Common color", Color(SkyBlockRarity.COMMON.color)).childOf { colorExpandable }
@@ -37,22 +43,34 @@ object ItemRarityBackground : Module(
 
     init {
         on<GuiEvent.Items.Render.Pre> {
-            if (item.isEmpty) return@on
-            val color = item.getData(DataTypes.RARITY)?.get() ?: return@on
+            graphics.fn(item, x, y)
+        }.runWhen(mode.state.map { it == 0 })
 
-            when (renderStyle) {
-                0 -> {
-                    graphics.drawRectangle(x, y, 16, 16, color.withAlpha(fillAlpha))
-                }
+        on<GuiEvent.Slots.Render.Pre> {
+            graphics.fn(slot.item ?: return@on, slot.x, slot.y)
+        }.runWhen(mode.state.map { it == 1 })
 
-                1 -> {
-                    graphics.drawOutline(x, y, 16, 16, 1, color, true)
-                }
+        on<GuiEvent.Slots.Render.Hotbar.Pre> {
+            graphics.fn(item, x, y)
+        }.runWhen(mode.state.map { it == 1 } and hotbar.state)
+    }
 
-                2 -> {
-                    graphics.drawRectangle(x, y, 16, 16, color.withAlpha(fillAlpha))
-                    graphics.drawOutline(x, y, 16, 16, 1, color, true)
-                }
+    private fun GuiGraphics.fn(item: ItemStack, x: Int, y: Int) {
+        if (item.isEmpty) return
+        val color = item.getData(DataTypes.RARITY)?.get() ?: return
+
+        when (renderStyle) {
+            0 -> {
+                drawRectangle(x, y, 16, 16, color.withAlpha(fill))
+            }
+
+            1 -> {
+                drawOutline(x, y, 16, 16, 1, color, true)
+            }
+
+            2 -> {
+                drawRectangle(x, y, 16, 16, color.withAlpha(fill))
+                drawOutline(x, y, 16, 16, 1, color, true)
             }
         }
     }
