05fdfe9869eaa944db7086d2c4b1d6603a91f3df
skies-starred <skies.starred@gmail.com>
2026-04-21 16:45:39 +0100

    feat(RenderOptimiser): hide armor

--- src/main/kotlin/xyz/aerii/athen/modules/impl/render/RenderOptimiser.kt
+++ src/main/kotlin/xyz/aerii/athen/modules/impl/render/RenderOptimiser.kt
@@ -1,5 +1,10 @@
 package xyz.aerii.athen.modules.impl.render
 
+import net.minecraft.client.player.LocalPlayer
+import net.minecraft.client.renderer.entity.state.AvatarRenderState
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
 import xyz.aerii.athen.annotations.Load
 import xyz.aerii.athen.config.Category
 import xyz.aerii.athen.events.WorldRenderEvent
@@ -13,6 +18,11 @@ object RenderOptimiser :  Module(
     Category.RENDER
 ) {
     private val _arm by config.switch("Hide player arm")
+
+    private val armor0 = config.switch("Hide armor on players").custom("_armor0")
+    private val armor0t by config.multiCheckbox("Players to hide for", listOf("Self", "Others"), listOf(0)).dependsOn { armor0.value }
+    private val armor1 = config.switch("Hide armor on all mobs").custom("_armor1")
+
     private val _glow by config.switch("Hide glowing effect")
     private val _fog by config.switch("Hide fog", true)
     private val lavaOverlay by config.switch("Hide lava overlay", true)
@@ -43,5 +53,31 @@ object RenderOptimiser :  Module(
         on<WorldRenderEvent.Entity.Pre> {
             renderState.displayFireAnimation = false
         }.runWhen(entityFire.state)
+
+        on<WorldRenderEvent.Entity.Pre> {
+            val r = renderState as? AvatarRenderState ?: return@on
+            val e = entity ?: return@on
+
+            val a = armor0t
+            val b = 0 in a && e is LocalPlayer
+            val c = 1 in a && e is Player && e !is LocalPlayer
+            if (!b && !c) return@on
+
+            r.fn0()
+        }.runWhen(armor0.state)
+
+        on<WorldRenderEvent.Entity.Pre> {
+            val r = renderState as? HumanoidRenderState ?: return@on
+            if (r is AvatarRenderState) return@on
+
+            r.fn0()
+        }.runWhen(armor1.state)
+    }
+
+    fun HumanoidRenderState.fn0() {
+        headEquipment = ItemStack.EMPTY
+        chestEquipment = ItemStack.EMPTY
+        legsEquipment = ItemStack.EMPTY
+        feetEquipment = ItemStack.EMPTY
     }
 }
