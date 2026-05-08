99eb8c7e43c3b6995a17ab27b0b7a9a1f94c9ed4
skies-starred <skies.starred@gmail.com>
2026-05-08 07:06:35 +0100

    feat(MinibossAlert): detection type

--- src/main/kotlin/xyz/aerii/athen/modules/impl/slayer/MinibossAlert.kt
+++ src/main/kotlin/xyz/aerii/athen/modules/impl/slayer/MinibossAlert.kt
@@ -3,10 +3,13 @@
 package xyz.aerii.athen.modules.impl.slayer
 
 import tech.thatgravyboat.skyblockapi.api.area.slayer.SlayerMiniBoss
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.findGroup
 import xyz.aerii.athen.annotations.Load
 import xyz.aerii.athen.annotations.OnlyIn
 import xyz.aerii.athen.config.Category
+import xyz.aerii.athen.events.MessageEvent
 import xyz.aerii.athen.events.SlayerEvent
+import xyz.aerii.athen.events.core.runWhen
 import xyz.aerii.athen.handlers.Notifier.notify
 import xyz.aerii.athen.handlers.Typo.modMessage
 import xyz.aerii.athen.modules.Module
@@ -21,6 +24,8 @@ object MinibossAlert : Module(
     "Shows an alert for you when a miniboss spawns nearby.",
     Category.SLAYER
 ) {
+    private val detection = config.dropdown("Detection type", listOf("Chat based", "Event based"), 1).custom("detection")
+    private val _unused by config.textParagraph("Chat based detection type only works for your minibosses. Event based detection type works for all minibosses near you.")
     private val sendMessage by config.switch("Send message", true)
     private val vanillaMessage by config.switch("Use mc message").dependsOn { sendMessage }
     private val showTitle by config.switch("Show title", true)
@@ -29,7 +34,18 @@ object MinibossAlert : Module(
     private val bigBoiText by config.textInput("Big boi text", "<red>Big boi spawned!")
     private val _unused0 by config.textParagraph("The same text will be used for both title and message.\n<gray>Big boi = Big miniboss")
 
+    private val bigBoys = SlayerMiniBoss.entries.filter { it.isBigBoy }.map { it.displayName }
+    private val regex = Regex("^SLAYER MINI-BOSS (?<name>.+?) has spawned!$")
+
     init {
+        on<MessageEvent.Chat.Receive> {
+            val name = regex.findGroup(stripped, "name") ?: return@on
+            val text = if (name in bigBoys) bigBoiText else alertText
+
+            if (showTitle) text.parse().alert()
+            if (sendMessage) if (vanillaMessage) text.parse().modMessage() else text.notify()
+        }.runWhen(detection.state.map { it == 0 })
+
         on<SlayerEvent.Miniboss.Spawn> {
             if (entity.tickCount >= 20) return@on
             val player = client.player ?: return@on
@@ -38,6 +54,6 @@ object MinibossAlert : Module(
 
             if (showTitle) text.parse().alert()
             if (sendMessage) if (vanillaMessage) text.parse().modMessage() else text.notify()
-        }
+        }.runWhen(detection.state.map { it == 1 })
     }
 }
