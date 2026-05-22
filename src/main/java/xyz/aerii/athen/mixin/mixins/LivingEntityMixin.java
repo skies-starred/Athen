92a5559354e231b9fcb5e98492b771183b75d630
skies-starred <skies.starred@gmail.com>
2026-05-22 13:46:52 +0100

    internal(LivingEntityMixin)

--- src/main/java/xyz/aerii/athen/mixin/mixins/LivingEntityMixin.java
+++ src/main/java/xyz/aerii/athen/mixin/mixins/LivingEntityMixin.java
@@ -1,24 +1,23 @@
 package xyz.aerii.athen.mixin.mixins;
 
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.aerii.athen.events.EntityEvent;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
-    public LivingEntityMixin(EntityType<?> entityType, Level level) {
-        super(entityType, level);
-    }
-
+public class LivingEntityMixin {
     @Inject(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;setPose(Lnet/minecraft/world/entity/Pose;)V"))
     private void athen$die(DamageSource damageSource, CallbackInfo ci) {
-        new EntityEvent.Death(this).post();
+        new EntityEvent.Death(athen$self()).post();
+    }
+
+    @Unique
+    private LivingEntity athen$self() {
+        return (LivingEntity) (Object) this;
     }
 }
