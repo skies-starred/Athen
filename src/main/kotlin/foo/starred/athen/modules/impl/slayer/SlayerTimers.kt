5e5db95e2ebd76591571bf215c5a4f738e1240c2
skies-starred <github@starred.foo>
2026-09-09 06:23:24 +0100

    feat(SlayerTimers): customisation

--- src/main/kotlin/foo/starred/athen/modules/impl/slayer/SlayerTimers.kt
+++ src/main/kotlin/foo/starred/athen/modules/impl/slayer/SlayerTimers.kt
@@ -1,3 +1,5 @@
+@file:Suppress("Unused")
+
 package foo.starred.athen.modules.impl.slayer
 
 import com.mojang.serialization.Codec
@@ -27,19 +29,32 @@ object SlayerTimers : Module(
     "Kill and spawn timers for slayer bosses.",
     Category.SLAYER
 ) {
+    private val style0 by config.input("Spawn style", "Slayer spawned in <yellow>#time<r>.")
+    private val _unused0 by config.variables("#time")
+
+    private val style1 by config.input("Kill style", "Slayer#special killed in #color#time <gray>| #pb<r>.")
+    private val _unused1 by config.variables("#special", "#color", "#time", "#pb", "#ticks")
+
+    private val group0 by config.group("PB styles", false)
+    private val pb0 by group0.input("First PB", "<yellow>#time <green>[New PB]")
+    private val pb1 by group0.input("Faster", "<green>#time <dark_green>[-#diff]")
+    private val pb2 by group0.input("Slower", "#color#time #color_diff[+#diff]")
+    private val _unused2 by group0.variables("#time", "#diff", "#pb", "#color", "#color_diff", "#ticks")
+
     private val json = JsonStore("features/slayerTimers")
-    private val killPBs = json.mutableMap("kill_pbs", Codec.STRING, Codec.DOUBLE)
-    private var questStart: Long = 0
-    private var startTick: Int = 0
+    private val kills = json.mutableMap("kill_pbs", Codec.STRING, Codec.DOUBLE)
+
+    private var start0: Long = 0
+    private var start1: Int = 0
     private var bool: Boolean = false
 
     init {
         on<SlayerEvent.Quest.Start> {
-            questStart = System.currentTimeMillis()
+            start0 = System.currentTimeMillis()
         }
 
         on<SlayerEvent.Quest.End> {
-            questStart = 0
+            start0 = 0
         }
 
         on<SlayerEvent.Reset.Any> {
@@ -53,12 +68,11 @@ object SlayerTimers : Module(
             if (bool && a) return@on ::bool.set(false)
             if (a) bool = true
 
-            startTick = Scheduler.ticks.server
-            val spawnTime = System.currentTimeMillis()
-            if (questStart <= 0) return@on
+            start1 = Scheduler.ticks.server
+            if (start0 <= 0) return@on
 
-            val time = (spawnTime - questStart) / 1000.0
-            "Slayer spawned in <yellow>${time.toDuration(secondsDecimals = 1)}<r>.".mod()
+            val time = (System.currentTimeMillis() - start0) / 1000.0
+            style0.replace("#time", time.toDuration(secondsDecimals = 1)).mod()
         }
 
         on<SlayerEvent.Boss.Death> {
@@ -66,33 +80,38 @@ object SlayerTimers : Module(
 
             val time = entity.tickCount / 20.0
             val str0 = time.toDuration(secondsDecimals = 1)
-            val time0 = Scheduler.ticks.server - startTick
+            val time0 = Scheduler.ticks.server - start1
             val str1 = (time0 / 20.0).toDuration(secondsDecimals = 1)
             val key = slayerInfo.string + if (entity.customName?.stripped()?.contains("Conjoined Brood") == true) "_P2" else ""
-            val pb = killPBs.value[key]
+            val pb = kills.value[key]
 
-            val str = when {
+            if (pb == null || time < pb) {
+                kills.update {
+                    this[key] = time
+                }
+            }
+
+            val (c, s) = when {
                 pb == null -> {
-                    killPBs.update { this[key] = time }
-                    "<yellow>$str0 <gray>| <yellow>$str1 <green>[New PB]"
+                    "<yellow>" to pb0.parse(str1, "", "", "<yellow>", "<green>", "$time0")
                 }
 
                 time < pb -> {
-                    killPBs.update { this[key] = time }
-                    "<green>$str0 <gray>| <green>$str1 <dark_green>[-${(pb - time).toDuration(secondsDecimals = 1)}]"
+                    "<green>" to pb1.parse(str1, (pb - time).toDuration(secondsDecimals = 1), pb.toDuration(secondsDecimals = 1), "<green>", "<dark_green>", "$time0")
                 }
 
                 else -> {
                     val a = time < pb * 1.1
-                    val color = if (a) Mocha.Peach.argb else TextColor.RED
-                    val tc = if (a) Mocha.Pink.argb else Mocha.Red.argb
-                    "<$color>$str0 <gray>| <$color>$str1 <$tc>[+${(time - pb).toDuration(secondsDecimals = 1)}]"
+                    val c0 = if (a) Mocha.Peach.argb else TextColor.RED
+                    val c1 = if (a) Mocha.Pink.argb else Mocha.Red.argb
+                    "<$c0>" to pb2.parse(str1, (time - pb).toDuration(secondsDecimals = 1), pb.toDuration(secondsDecimals = 1), "<$c0>", "<$c1>", "$time0")
                 }
             }
 
             val a = slayerInfo.type == SlayerBoss.Tarantula && slayerInfo.tier == SlayerTier.Five
             val p = if (a && bool) " <dark_gray>[P1]<r>" else if (a) " <dark_gray>[P2]<r>" else ""
-            "<hover:<red>$time0 ticks.>Slayer$p killed in $str<r>.".mod()
+            val string = style1.replace("#special", p).replace("#color", c).replace("#time", str0).replace("#pb", s).replace("#ticks", "$time0")
+            "<hover:<red>$time0 ticks.>$string".mod()
         }
 
         command {
@@ -102,7 +121,7 @@ object SlayerTimers : Module(
 
                 b0.lie()
 
-                val a = killPBs.value.entries.groupBy { it.key.substringBeforeLast("_T") }
+                val a = kills.value.entries.groupBy { it.key.substringBeforeLast("_T") }
                 var f = true
 
                 for (type in a.keys.sorted()) {
@@ -123,11 +142,23 @@ object SlayerTimers : Module(
         }
     }
 
-    private fun String.str(): String =
-        lowercase().split("_").joinToString(" ") { word -> word.replaceFirstChar { it.uppercase() } }
+    private fun String.str(): String {
+        return lowercase().split("_").joinToString(" ") { word -> word.replaceFirstChar { it.uppercase() } }
+    }
+
+    private fun String.parse(time: String, diff: String, pb: String, color: String, color1: String, ticks: String): String {
+        return replace("#color_diff", color1)
+            .replace("#diff_color", color1)
+            .replace("#color1", color1)
+            .replace("#color", color)
+            .replace("#diff", diff)
+            .replace("#time", time)
+            .replace("#pb", pb)
+            .replace("#ticks", ticks)
+    }
 
     private fun reset() {
         bool = false
-        questStart = 0
+        start0 = 0
     }
 }
